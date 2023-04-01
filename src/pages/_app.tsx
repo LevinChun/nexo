401b450860f19f1a685c5667cd8b44bcde198a07
LevinChun <96810516+LevinChun@users.noreply.github.com>
2023-04-01 21:16:49 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={client}>
         <Head>
-          <title>Demo Bot</title>
+          <title>Nexo Bot</title>
         </Head>
         {getLayout(<Component {...pageProps} />)}
       </QueryClientProvider>
